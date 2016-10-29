58210e852b9318e96aff5e0998092ca7aa6845e8
fdurant <frederik.durant@pandora.be>
2016-10-29 17:38:37 +0200

    Added program parameters for sheet name and column number

--- src/build_firstname_network.py
+++ src/build_firstname_network.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import argparse
 import sys
 import networkx as nx
@@ -6,19 +7,37 @@ from xlrd import open_workbook
 def init():
     parser = argparse.ArgumentParser()
     parser.add_argument('--inFileXls', dest='inFileXls', 
-                        type=str,
-                        help='input file with Belgian first names in MS Excel format',
+                        type=str, nargs='+',
+                        help='input file(s) with Belgian first names in MS Excel format',
                         required=True)
+    parser.add_argument('--sheetName', dest='sheetName', 
+                        type=str, default='1995-2015',
+                        help='name of the sheet - i.e. year - of interest, e.g. 2000 (default is 1995 through 2015)')
+    parser.add_argument('--partOfCountry', dest='partOfCountry', 
+                        type=int, default=1,
+                        help='1 = whole of Belgium (DEFAULT); 2 = Brussels only; 3=Flanders only; 4=Wallonia only)')
     parser.add_argument('--outFileGraphML', dest='outFileGraphML', 
                         type=str,
                         help='output file with Belgian first names in GraphML format',
                         required=True)
     global args
     args = vars(parser.parse_args())
+    print args
 
-def read_file(xlsFileName):
-    wb = open_workbook(filename=xlsFileName)
-    pass
+def read_file(fileList):
+    
+    # Internal mapping
+    region2column = {1:1,
+                     2:4,
+                     3:7,
+                     4:10}
+
+    for fn in fileList:
+        wb = open_workbook(filename=fn)
+        sheet = wb.sheet_by_name(args['sheetName'])
+        columnNumber = region2column[args['partOfCountry']]
+        
+        // CONTINUE HERE
 
 def build_network():
     pass
